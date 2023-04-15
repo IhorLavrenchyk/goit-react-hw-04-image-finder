cd45a29f1117ced33b44ae1f68c8aa9d5aba1d70
IHOR LAVRENCHUK <110937655+IhorLavrenchyk@users.noreply.github.com>
2023-04-16 01:22:03 +0300

    Initial commit

---
+++ src/components/services/settings.js
@@ -0,0 +1,9 @@
+// Базові налаштування URL
+const settings = {
+  BASE_URL: 'https://pixabay.com/api/?',
+  API_KEY: 'key=33369159-12dbd94a1e76cccc8767e4305',
+  TYPE: '&image_type=photo',
+  PER_PAGE: '&per_page=12',
+};
+
+export default settings;
